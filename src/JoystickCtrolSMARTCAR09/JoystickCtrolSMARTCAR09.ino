c15ef04fe0e0187666008c357031e499121d3653
Lukasz Kapiec <lkapiec@gmail.com>
2024-01-13 17:46:24 +0100

    Update JoystickCtrolSMARTCAR09.ino

--- src/JoystickCtrolSMARTCAR09/JoystickCtrolSMARTCAR09.ino
+++ src/JoystickCtrolSMARTCAR09/JoystickCtrolSMARTCAR09.ino
@@ -6,26 +6,16 @@
 // Buy from: https://open-smart.aliexpress.com/store/1199788
 -------------------------------------*/
 
-#define RH
-
 #include <String.h>
 #include "QuadMotorDriver.h"
 
-#ifdef RH
 #include "RH_ASK.h"
-#else
-#include <VirtualWire.h>
-#endif
 
-int RF_RX_PIN = 9;            //the RF433 or RF315 receiver module connect with D9 pin of Easy IO Shield pro.
 void (*resetFunc)(void) = 0;  //software reset when the RF receiver can not respond
 
-#ifdef RH
+int RF_RX_PIN = 9;            //the RF433 or RF315 receiver module connect with D9 pin of Easy IO Shield pro.
 int RF_TX_PIN = 10;  //fake pin
 RH_ASK driver(2000, RF_RX_PIN, RF_TX_PIN, 0);
-#endif
-
-#define REMOTE_CONTROLER_ID 0
 
 //Remote control code macro
 #define CMD_INVALID 0
@@ -54,33 +44,21 @@ uint8_t speed0 = 200;  //Initial value of speed, value should 0~255.
 
 void setup() {
   Serial.begin(115200);
+  
   motordriver.init(PWMA, PWMB, PWMC, PWMD);
   motordriver.setSpeed(speed0);
-#ifdef RH
+  
   if (!driver.init())
     Serial.println("init failed");
-#else
-  vw_set_rx_pin(RF_RX_PIN);             // Setup receive pin.
-  vw_setup(2000);                       // Transmission speed in bits per second.
-  vw_rx_start();                        // Start the PLL receiver.
-#endif
 }
 
-#ifdef RH
 #define MAX_MESSAGE_LEN RH_ASK_MAX_MESSAGE_LEN
-#else
-#define MAX_MESSAGE_LEN VW_MAX_MESSAGE_LEN
-#endif
 
 uint8_t buffer[MAX_MESSAGE_LEN];  // buffer array for data recieve over serial port
 uint8_t buflen = MAX_MESSAGE_LEN;
 
 void loop() {
-#ifdef RH
   if (driver.recv(buffer, &buflen))  // Non-blocking
-#else
-  if (vw_get_message(buffer, &buflen))  // non-blocking I/O
-#endif
   {
     uint8_t command;
     command = decode();
@@ -152,9 +130,7 @@ uint8_t decode()
   uint8_t button;
   uint8_t joystic_button;
   uint8_t command = CMD_INVALID;
-  
-
-  
+    
   if ((buffer[0] != 0x7E) || (buffer[4] != 0xEF)) {
     resetFunc();  //software reset
     return command;
@@ -166,11 +142,10 @@ uint8_t decode()
   button = buffer[3] & 0x07;
   joystic_button = (buffer[3] >> 3) & 1;
 
-  if ((x < (X0 + XY_TOLERANCE)) && (x > (X0 - XY_TOLERANCE))) {
-    if (y > 800)
-      command = CMD_FORWARD;
-    else if (y < 200)
-      command = CMD_BACKWARD;
+  if ((x < (X0 + XY_TOLERANCE)) && (x > (X0 - XY_TOLERANCE))) 
+  {
+    if (y > 800) command = CMD_FORWARD;
+    else if (y < 200) command = CMD_BACKWARD;
     else command = CMD_STOP;
   } else if (x < (X0 - XY_TOLERANCE)) 
   {
@@ -181,7 +156,9 @@ uint8_t decode()
     if ((y < (Y0 + XY_TOLERANCE)) && (y > (Y0 - XY_TOLERANCE)))
       command = CMD_SLIDE_RIGHT;
   }
-  switch (button) {
+
+  switch (button) 
+  {
     case 1: command = CMD_SPEED_UP; break;    //K1 button;
     case 2: command = CMD_TURN_RIGHT; break;  //K2 button;
     case 3: command = CMD_SPEED_DOWN; break;  //K3 button;
