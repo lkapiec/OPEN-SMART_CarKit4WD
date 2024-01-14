1c9512818bbaf3ad1f5c5815d995e0486d79460e
Lukasz Kapiec <lkapiec@gmail.com>
2024-01-15 00:02:39 +0100

    cleaning code

--- src/JoystickCtrolSMARTCAR09/JoystickCtrolSMARTCAR09.ino
+++ src/JoystickCtrolSMARTCAR09/JoystickCtrolSMARTCAR09.ino
@@ -1,34 +1,35 @@
 /*------------------------------------
-// Function: This is car control demo code for Wireless Joystick Remote 
-	to control the smart car.
+// Function: This is car control demo code for Wireless Joystick Remote to control the smart car.
 // IDE: Arduinio-1.8.7
 // Author: OPEN-SMART Team
 // Buy from: https://open-smart.aliexpress.com/store/1199788
 -------------------------------------*/
 
-#include <String.h>
 #include "QuadMotorDriver.h"
-
 #include "RH_ASK.h"
 
-void (*resetFunc)(void) = 0;  //software reset when the RF receiver can not respond
+void (*resetFunc)(void) = 0; //software reset when the RF receiver can not respond
 
-int RF_RX_PIN = 9;            //the RF433 or RF315 receiver module connect with D9 pin of Easy IO Shield pro.
+int RF_RX_PIN = 9;   //the RF433 or RF315 receiver module connect with D9 pin of Easy IO Shield pro.
 int RF_TX_PIN = 10;  //fake pin
-RH_ASK driver(2000, RF_RX_PIN, RF_TX_PIN, 0);
+#define TRANSMISION_SPEED 2000
+RH_ASK rh_driver(TRANSMISION_SPEED, RF_RX_PIN, RF_TX_PIN, 0);
 
 //Remote control code macro
-#define CMD_INVALID 0
-#define CMD_FORWARD 1
-#define CMD_TURN_RIGHT 2
-#define CMD_SLIDE_RIGHT 3
-#define CMD_BACKWARD 4
-#define CMD_TURN_LEFT 5
-#define CMD_SLIDE_LEFT 6
-#define CMD_STOP 7
-#define CMD_SPEED_UP 8
-#define CMD_SPEED_DOWN 9
-#define CMD_JOYSTIC_BUTTON 10
+//#define CMD_INVALID 0
+//#define CMD_FORWARD 1
+//#define CMD_TURN_RIGHT 2
+//#define CMD_SLIDE_RIGHT 3
+//#define CMD_BACKWARD 4
+//#define CMD_TURN_LEFT 5
+//#define CMD_SLIDE_LEFT 6
+//#define CMD_STOP 7
+//#define CMD_SPEED_UP 8
+//#define CMD_SPEED_DOWN 9
+//#define CMD_JOYSTIC_BUTTON 10
+
+enum COMMAND_CONTROL {CMD_INVALID = 0, CMD_FORWARD, CMD_TURN_RIGHT, CMD_SLIDE_RIGHT, CMD_BACKWARD, 
+CMD_TURN_LEFT, CMD_SLIDE_LEFT,CMD_STOP, CMD_SPEED_UP,CMD_SPEED_DOWN,CMD_JOYSTIC_BUTTON};
 
 #define XY_TOLERANCE 50
 #define X0 510  //Initial value of X-asix of the Joystick
@@ -42,23 +43,24 @@ uint8_t speed0 = 200;  //Initial value of speed, value should 0~255.
 #define PWMB 5
 #define PWMA 3
 
-void setup() {
+#define MAX_MESSAGE_LEN RH_ASK_MAX_MESSAGE_LEN
+uint8_t buflen = MAX_MESSAGE_LEN;
+uint8_t buffer[MAX_MESSAGE_LEN];  // buffer array for data recieve over serial port
+
+void setup() 
+{
   Serial.begin(115200);
   
   motordriver.init(PWMA, PWMB, PWMC, PWMD);
   motordriver.setSpeed(speed0);
   
-  if (!driver.init())
-    Serial.println("init failed");
+  if (!rh_driver.init())
+    Serial.println("RadioHead driver initialization failed!");
 }
 
-#define MAX_MESSAGE_LEN RH_ASK_MAX_MESSAGE_LEN
-
-uint8_t buffer[MAX_MESSAGE_LEN];  // buffer array for data recieve over serial port
-uint8_t buflen = MAX_MESSAGE_LEN;
-
-void loop() {
-  if (driver.recv(buffer, &buflen))  // Non-blocking
+void loop() 
+{
+  if(rh_driver.recv(buffer, &buflen))  // Non-blocking
   {
     uint8_t command;
     command = decode();
@@ -70,53 +72,65 @@ void loop() {
 
 void controlCar(uint8_t cmd) 
 {  
-  switch (cmd) {
+  switch (cmd) 
+  {
     case CMD_FORWARD:
       motordriver.goForward();
       break;
+    
     case CMD_TURN_RIGHT:
       motordriver.turnRight();
       delay(100);
       motordriver.stop();
       break;
+    
     case CMD_SLIDE_RIGHT:
       motordriver.slideRight();
       delay(200);
       break;
+    
     case CMD_BACKWARD: 
       motordriver.goBackward(); 
       break;
+    
     case CMD_SLIDE_LEFT:
       motordriver.slideLeft();
       delay(200);
       break;
+    
     case CMD_TURN_LEFT:
       motordriver.turnLeft();
       delay(100);
       motordriver.stop();
       break;
+    
     case CMD_STOP: 
       motordriver.stop(); 
       break;
+    
     case CMD_SPEED_UP: 
       speedUp(); 
       break;
+    
     case CMD_SPEED_DOWN: 
       speedDown(); 
       break;
+    
     default: 
       break;
   }
 }
 
-void speedUp() {
+void speedUp() 
+{
   if (speed0 < 236) 
     speed0 += SPEED_STEPS;
   else speed0 = 255;
   motordriver.setSpeed(speed0);
 }
 
-void speedDown() {
+void speedDown() 
+{
   if (speed0 > 70) speed0 -= SPEED_STEPS;
   else speed0 = 50;
   motordriver.setSpeed(speed0);
@@ -131,7 +145,8 @@ uint8_t decode()
   uint8_t joystic_button;
   uint8_t command = CMD_INVALID;
     
-  if ((buffer[0] != 0x7E) || (buffer[4] != 0xEF)) {
+  if ((buffer[0] != 0x7E) || (buffer[4] != 0xEF)) 
+  {
     resetFunc();  //software reset
     return command;
   }
@@ -140,7 +155,7 @@ uint8_t decode()
   y = buffer[2] << 2;
   z = buffer[3] >> 3;
   button = buffer[3] & 0x07;
-  joystic_button = (buffer[3] >> 3) & 1;
+  joystic_button = z & 1;
 
   if ((x < (X0 + XY_TOLERANCE)) && (x > (X0 - XY_TOLERANCE))) 
   {
@@ -159,11 +174,20 @@ uint8_t decode()
 
   switch (button) 
   {
-    case 1: command = CMD_SPEED_UP; break;    //K1 button;
-    case 2: command = CMD_TURN_RIGHT; break;  //K2 button;
-    case 3: command = CMD_SPEED_DOWN; break;  //K3 button;
-    case 4: command = CMD_TURN_LEFT; break;   //K4 button;
-    default: break;
+    case 1: //K1 button
+      command = CMD_SPEED_UP; 
+      break;    
+    case 2: //K2 button;
+      command = CMD_TURN_RIGHT; 
+      break;
+    case 3: //K3 button;
+      command = CMD_SPEED_DOWN; 
+      break;
+    case 4: //K4 button;
+      command = CMD_TURN_LEFT; 
+      break;
+    default: 
+      break;
   }
   
   clearBufferArray();
@@ -174,7 +198,5 @@ uint8_t decode()
 void clearBufferArray()  // function to clear buffer array
 {
   for (int i = 0; i < buflen; i++) 
-  { 
     buffer[i] = 0; 
-  }  // clear all index of array with 0
 }
--- src/WirelessTransmitterFirmware/WirelessTransmitterFirmware.ino
+++ src/WirelessTransmitterFirmware/WirelessTransmitterFirmware.ino
@@ -1,46 +1,60 @@
+#define RH
+#ifdef RH
+#include "RH_ASK.h"
+#else
 #include <VirtualWire.h>
+#endif
 #include <OS_JoystickCtrl.h>
 
 JoystickCtrl remote;
-int RF_TX_PIN = 6;//connect the sent module to D2 to use  
-              //you can change it to the idle port you want. 
- 
-void setup()
-{
+int RF_TX_PIN = 6;  //connect the sent module to D2 to use
+                    //you can change it to the idle port you want.
+#define TRANSMISION_SPEED 2000
+
+#ifdef RH
+int RF_RX_PIN = 10;  //fake pin
+RH_ASK driver(TRANSMISION_SPEED, RF_RX_PIN, RF_TX_PIN, 0);
+#endif
+
+void setup() {
   remote.init();
-  vw_set_tx_pin(RF_TX_PIN); // Setup transmit pin
-  vw_setup(2000); // Transmission speed in bits per second.
+
+#ifdef RH
+  if (!driver.init())
+    Serial.println("init failed");
+#else
+  vw_set_tx_pin(RF_TX_PIN);          // Setup transmit pin
+  vw_setup(TRANSMISION_SPEED);       // Transmission speed in bits per second.
+#endif
 }
 
-uint8_t buffer[5]= {0X7E,0,0,0,0XEF}; // buffer array for data recieve over serial port 
+#define BUFFER_LENGHT 5
+uint8_t buffer[BUFFER_LENGHT] = { 0X7E, 0, 0, 0, 0XEF };  // buffer array for data recieve over serial port
 
-void loop()
-{
-  
-   
-    
- int x,y; 
- uint8_t xh,xl,yh,yl;
+void loop() {
+  int x, y;
+  uint8_t xh, xl, yh, yl;
   uint8_t z;
-  if(remote.isChange())
-  {
-    x=remote.nowX;
-	x = x>>2;
-    y=remote.nowY;
-    y = y>>2;
-    z=remote.nowZ;
-	z <<= 3;
+
+  if (remote.isChange()) {
+    x = remote.nowX;
+    x = x >> 2;
+    y = remote.nowY;
+    y = y >> 2;
+    z = remote.nowZ;
+    z <<= 3;
     uint8_t button = remote.nowkey;
     buffer[1] = x;
     buffer[2] = y;
-	buffer[3] = z|button;
+    buffer[3] = z | button;
 
-  vw_send(buffer, 5);  // 
-    delay(100);//between two message, you should delay 400ms so that the receiver may not miss it.
+#ifdef RH
+    driver.send(buffer, BUFFER_LENGHT);
+    driver.waitPacketSent();
+    delay(200);
+#else
+    vw_send(buffer, BUFFER_LENGHT);  //
+    delay(100);                      //between two message, you should delay 400ms so that the receiver may not miss it.
+#endif
   }
-
- 
 }
-
-
-
